9822218e918f5f6d854dbe6493767b001a2f35b2
Fabiano Júnior Lima Barbosa <138934847+Fabito02@users.noreply.github.com>
2024-08-25 16:39:15 -0300

    Add files via upload

--- Mapa_Interativo/source/gerar-elementos.js
+++ Mapa_Interativo/source/gerar-elementos.js
@@ -1,22 +1,53 @@
 const estados = document.querySelectorAll('.estado');
 const key = '00d597d79abf4aaebac53307242208';
 const lang = 'pt';
+var estadosData = {};
 
-document.addEventListener('DOMContentLoaded', () => {
-let elementoFlutuante = document.getElementById('elementoFlutuante');
-    let estadosData = {};
+// carrega os dados do JSON
+function carregarEstados() {
+    return fetch('source/dados-estados.json')
+        .then(response => response.json())
+        .then(dados => {
+            estadosData = dados;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar estados:', error);
+        });
+}
 
-    // carrega os dados do JSON
-    function carregarEstados() {
-        return fetch('source/dados-estados.json')
+const tempoAtualizacao = 5 * 60 * 1000; // 5 minutos
+const cacheClima = {};
+
+// Pequeno script para criar um cache para os climas e diminuir as chamadas na API
+
+function getClima(city) {
+    console.log()
+    const agora = new Date().getTime();
+    if (cacheClima[city] && (agora - cacheClima[city].timestamp < tempoAtualizacao)) {
+        return Promise.resolve(cacheClima[city].data);  // Retorna uma Promise resolvida com os dados em cache
+    } else {
+        const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&lang=${lang}`;
+        return fetch(url)
             .then(response => response.json())
-            .then(dados => {
-                estadosData = dados;
+            .then(data => {
+                cacheClima[city] = {
+                    data: data,
+                    timestamp: agora
+                };
+                console.log(`Cache do clima da cidade ${city} atualizado`)
+                return data;
             })
             .catch(error => {
-                console.error('Erro ao carregar estados:', error);
+                console.error('Erro:', error);
+                throw error;
             });
     }
+}
+
+carregarEstados()
+
+document.addEventListener('DOMContentLoaded', () => {
+    let elementoFlutuante = document.getElementById('elementoFlutuante');
 
     // carregar os dados dos estados
     carregarEstados().then(() => {
@@ -26,23 +57,17 @@ let elementoFlutuante = document.getElementById('elementoFlutuante');
                 let estadoData = estadosData[code];
 
                 if (estadoData) {
-
-                    // informações climáticas
-                    city = estadoData.city
+                    let city = estadoData.city;
                     
-                    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&lang=${lang}`;
                     if (city) {
-                        fetch(url)
-                            .then(response => response.json())
-                            .then(data => {
-                                const iconUrl = data.current.condition.icon;
-                                const clima = `<img src="${iconUrl}" alt="Ícone do clima atual" class="iconClima icon"> <marquee scrollamount="5" class="textoElementoFlutuante textoClima">${data.current.temp_c}°C, ${data.current.condition.text} - ${city}</marquee>`;
-                                elementoFlutuante.querySelector('#clima').innerHTML = clima;
-                            })
-                            .catch(error => {
-                                console.error('Erro:', error);
-                                elementoFlutuante.querySelector('#clima').innerHTML = '<p style="color: red">erro ao obter o clima!</p>';
-                            });
+                        getClima(city).then(data => {
+                            const iconUrl = data.current.condition.icon;
+                            const clima = `<img src="${iconUrl}" alt="Ícone do clima atual" class="iconClima icon"> <marquee scrollamount="5" class="textoElementoFlutuante textoClima">${data.current.temp_c}°C, ${data.current.condition.text} - ${city}</marquee>`;
+                            elementoFlutuante.querySelector('#clima').innerHTML = clima;
+                        }).catch(error => {
+                            console.error('Erro:', error);
+                            elementoFlutuante.querySelector('#clima').innerHTML = '<p style="color: red">erro ao obter o clima!</p>';
+                        });
                     } else {
                         elementoFlutuante.querySelector('#clima').innerHTML = '';
                     }
@@ -55,7 +80,7 @@ let elementoFlutuante = document.getElementById('elementoFlutuante');
                     elementoFlutuante.querySelector('#biomas').innerHTML = estadoData.biomas;
                     elementoFlutuante.querySelector('#fontesEnergeticas').innerHTML = estadoData.fontesEnergeticas;
 
-                    // Mostrar o elemento flutuante
+                    // mostrar o elemento flutuante que segue o mouse
                     elementoFlutuante.classList.add('show');
                 }
             });
@@ -75,11 +100,7 @@ let elementoFlutuante = document.getElementById('elementoFlutuante');
     });
 });
 
-
-
-
 function abrirCaixasFlutuantes() {
-    let estadosData = {};
     let informacoesBase = document.querySelector('.conteudoInformacoesBase');
 
     // Exibir caixas flutuantes e botão de fechar
@@ -88,63 +109,44 @@ function abrirCaixasFlutuantes() {
     });
     document.querySelector('.fecharCaixasFlutuantes').classList.remove('hide');
 
-    // Função para carregar os estados
-    function carregarEstados() {
-        return fetch('source/dados-estados.json')
-            .then(response => response.json())
-            .then(dados => {
-                estadosData = dados;
-                return dados;
-            });
-    }
-
     // carregar os dados dos estados
-    carregarEstados().then(estados => {
-        document.querySelectorAll('.estado').forEach(estado => {
-            estado.addEventListener('click', () => {
-                let code = estado.getAttribute('code');
-                let estadoData = estadosData[code];
+    document.querySelectorAll('.estado').forEach(estado => {
+        estado.addEventListener('click', () => {
+            let code = estado.getAttribute('code');
+            let estadoData = estadosData[code];
 
-                if (estadoData) {
-                    // informações climáticas
-                    let city = estadoData.city;
-                    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&lang=${lang}`;
+            if (estadoData) {
+                let city = estadoData.city;
 
-                    if (city) {
-                        fetch(url)
-                            .then(response => response.json())
-                            .then(data => {
-                                const iconUrl = data.current.condition.icon;
-                                const clima = `<img src="${iconUrl}" alt="Ícone do clima atual" class="iconClima icon"> <marquee scrollamount="5" class="textoElementoFlutuante textoClima">${data.current.temp_c}°C, ${data.current.condition.text} - ${city}</marquee>`;
-                                informacoesBase.querySelector('#clima').innerHTML = clima;
-                            })
-                            .catch(error => {
-                                console.error('Erro:', error);
-                                informacoesBase.querySelector('#clima').innerHTML = '<p style="color: red">erro ao obter o clima!</p>';
-                            });
-                    } else {
-                        informacoesBase.querySelector('#clima').innerHTML = '';
-                    }
-
-                    // outros dados do estado
-                    informacoesBase.querySelector('#territorio').innerHTML = estadoData.extensaoTerritorial;
-                    informacoesBase.querySelector('#habitantes').innerHTML = estadoData.habitantes;
-                    informacoesBase.querySelector('#biomas').innerHTML = estadoData.biomas;
-                    informacoesBase.querySelector('#fontesEnergeticas').innerHTML = estadoData.fontesEnergeticas;
-
-                    topicos = document.querySelector('.topicos')
-
-                    topicos.querySelector('.biomasTopico').innerHTML = estadoData.biomasTopico
-                    topicos.querySelector('.fontesEnergeticasTopico').innerHTML = estadoData.fontesEnergeticasTopico
-                    topicos.querySelector('.reservasBiologicasTopico').innerHTML = estadoData.reservasBiologicasTopico
-                    topicos.querySelector('.reservasHidricasTopico').innerHTML = estadoData.reservasHidricasTopico
-                    topicos.querySelector('.cultura').innerHTML = estadoData.cultura
-                    topicos.querySelector('.historia').innerHTML = estadoData.historia
-                    topicos.querySelector('.questoesGeograficas').innerHTML = estadoData.questoesGeograficas
-                    topicos.querySelector('.impactosAmbientais').innerHTML = estadoData.impactosAmbientais
+                if (city) {
+                    getClima(city).then(data => {
+                        const iconUrl = data.current.condition.icon;
+                        const clima = `<img src="${iconUrl}" alt="Ícone do clima atual" class="iconClima icon"> <marquee scrollamount="5" class="textoElementoFlutuante textoClima">${data.current.temp_c}°C, ${data.current.condition.text} - ${city}</marquee>`;
+                        informacoesBase.querySelector('#clima').innerHTML = clima;
+                    }).catch(error => {
+                        console.error('Erro:', error);
+                        informacoesBase.querySelector('#clima').innerHTML = '<p style="color: red">erro ao obter o clima!</p>';
+                    });
+                } else {
+                    informacoesBase.querySelector('#clima').innerHTML = '';
                 }
-            });
+
+                // outros dados do estado
+                informacoesBase.querySelector('#territorio').innerHTML = estadoData.extensaoTerritorial;
+                informacoesBase.querySelector('#habitantes').innerHTML = estadoData.habitantes;
+                informacoesBase.querySelector('#biomas').innerHTML = estadoData.biomas;
+                informacoesBase.querySelector('#fontesEnergeticas').innerHTML = estadoData.fontesEnergeticas;
+
+                let topicos = document.querySelector('.topicos');
+                topicos.querySelector('.biomasTopico').innerHTML = estadoData.biomasTopico;
+                topicos.querySelector('.fontesEnergeticasTopico').innerHTML = estadoData.fontesEnergeticasTopico;
+                topicos.querySelector('.reservasBiologicasTopico').innerHTML = estadoData.reservasBiologicasTopico;
+                topicos.querySelector('.reservasHidricasTopico').innerHTML = estadoData.reservasHidricasTopico;
+                topicos.querySelector('.cultura').innerHTML = estadoData.cultura;
+                topicos.querySelector('.historia').innerHTML = estadoData.historia;
+                topicos.querySelector('.questoesGeograficas').innerHTML = estadoData.questoesGeograficas;
+                topicos.querySelector('.impactosAmbientais').innerHTML = estadoData.impactosAmbientais;
+            }
         });
     });
 }
-
